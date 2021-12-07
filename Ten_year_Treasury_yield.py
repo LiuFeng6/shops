520ef52cb006299ac1548055946d860b8522311c
LiuFeng6 <1336007004@qq.com>
2021-12-07 15:35:29 +0800

    完成10年期国债收益获取

---
+++ Ten_year_Treasury_yield.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
+}
+# url = 'https://cn.investing.com/rates-bonds/china-10-year-bond-yield?__cf_chl_jschl_tk__=FAl1tnF7Y6nQ7Y7FzlOPvf5md4RPpqGwueDZwJJSWCU-1638859732-0-gaNycGzNDFE'
+url = 'https://cn.investing.com/rates-bonds/china-10-year-bond-yield'
+response = requests.get(url, headers=headers).text
+china_10_year_bond_yield = BeautifulSoup(response, 'html.parser').find("span", id="fl_header_pair_lst").text
+
+
+print(china_10_year_bond_yield)
