5d1aae9744c8ce81e9d02f4d2ae57048913e5d03
LiuFeng6 <1336007004@qq.com>
2021-12-07 14:40:11 +0800

    完成pe获取

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
-url = 'http://value500.com/pe.asp'
+
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
 }
-response = requests.get(url, headers=headers).text    #
+url = 'http://value500.com/pe.asp'
+response = requests.get(url, headers=headers).text  #
 
-bb=BeautifulSoup(response,'html.parser')
-print(bb)
+treeak = BeautifulSoup(response, 'html.parser').find("body").find_all("tr", align="center")[1]
+PE_Ratio_shanghai = treeak.find_all("td")[1].text
+PE_Ratio_shenzheng = treeak.find_all("td")[2].text
+print(PE_Ratio_shanghai,PE_Ratio_shenzheng)
