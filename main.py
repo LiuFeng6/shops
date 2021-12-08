01a92a9d088d12c137d333ee1f520bffca333bf9
LiuFeng6 <1336007004@qq.com>
2021-12-08 16:43:08 +0800

    pe and 10years需要重写1

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ china_10_year_bond = 2.8 #china_10_year_bond_yield()
 stock_code_list = ['sh600000',  'sz300124', 'sh600276', 'sz002952']
 
 for i in stock_code_list:
-
     pe_ttm, dividend_rate = pe_ttm_all(i)
     print(i,"市盈率:",pe_ttm," ", "15")                         # 市盈率(TTM)： 是否小于15
     print("深圳市盈率:",PE_Ratio_shenzheng," ", "20")            # 深圳市盈率(TTM)： 是否小于20
